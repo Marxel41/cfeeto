d973aa7bed0154ee11490730651aea707453e346
Marxel41 <76850771+Marxel41@users.noreply.github.com>
2025-09-03 22:07:57 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // WICHTIG: Ersetze 'DEIN-REPO-NAME' mit dem Namen deines GitHub-Repositorys!
+  // Beispiel: base: '/brettspiel-palast/',
+  base: '/DEIN-REPO-NAME/',
+})
